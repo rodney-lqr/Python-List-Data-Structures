22dfd6932eea09300ac799dea821bf5129df08e7
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 20:04:39 +0800

    added a vegetable lists

---
+++ vegetableList.py
@@ -0,0 +1,22 @@
+vegetableList = [
+    'Carrot',
+    'Broccoli',
+    'Cauliflower',
+    'Potato',
+    'Tomato',
+    'Cucumber',
+    'Pepper'
+]
+
+print()
+print(vegetableList)
+print()
+print("The 5th index is: ", vegetableList[5])
+print()
+vegetableList[3] = "Spinach"
+print()
+vegetableList.remove(vegetableList[6])
+print(vegetableList)
+print()
+print(vegetableList[-1])
+print()
