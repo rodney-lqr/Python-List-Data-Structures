141558d8a2c7581505ec678790a1e2979557f506
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 19:30:29 +0800

    Creating a student name list and modifying the items on each lists

---
+++ studName.py
@@ -0,0 +1,24 @@
+#Creating and adding items from the list
+groupOne = ["Mark", "James", "Walter", "Perci", "Joana"]
+groupTwo = ["Rodney", "Danica", "Jayson", "Leon", "Blake"]
+groupThree = ["Harold", "Kobs", "Jenny", "Marie", "Chris"]
+#Concatenating all the groups together
+allStudents = groupOne + groupTwo + groupThree
+#Printing index number 11
+allStudents[11] = "John"
+print()
+print()
+#Printing the 15th index of the list
+print("The 15th index is: ", allStudents[-2])
+print()
+print()
+#Modified the item in index number 11
+print("The updated student number 10 is: ", allStudents[11])
+print()
+print()
+#Deleting the 9th index
+allStudents.remove(allStudents[9])
+print()
+print()
+print(allStudents)
+
