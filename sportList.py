028f4490c398420d150c1d51491f10074db3e541
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 23:53:15 +0800

    adding a lists of popular sports in the world

---
+++ sportList.py
@@ -0,0 +1,24 @@
+popularSports = [
+    "Soccer",
+    "Badminton",
+    "Tennis",
+    "Volleyball",
+    "Baseball",
+    "American football",
+    "Cricket",
+    "Hockey",
+    "Golf",
+    "Swimming"
+]
+
+print()
+print(popularSports)
+print()
+print(popularSports[5])
+popularSports[3] = "Basketball"
+del popularSports[8]
+for x in popularSports:
+    print(x)
+print()
+print(popularSports[-1])
+print()
