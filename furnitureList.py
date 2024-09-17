bc3c671ab48f67c7c045771c5ad3dcb32632313e
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 23:45:35 +0800

    adding a furniture item lists

---
+++ furnitureList.py
@@ -0,0 +1,32 @@
+furnitureItems = [
+    "Loveseat",
+    "Bed",
+    "Table",
+    "Chair",
+    "Wardrobe",
+    "Dresser",
+    "Bookshelf",
+    "Couch",
+    "Desk",
+    "Kitchen cabinet",
+    "Dining table",
+    "Bedside table"
+]
+
+print()
+print(furnitureItems)
+print()
+print("The 8th index is: ", furnitureItems[7])
+print()
+furnitureItems[4] = "Sofa"
+del furnitureItems[9]
+for x in furnitureItems:
+    print(x)
+
+print()
+print("The last index is: ", furnitureItems[-1])
+print()
+
+
+
+
