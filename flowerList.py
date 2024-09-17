5d747b31a037d4d53439e44db2347ba67be18b54
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 21:01:41 +0800

    added the flower lists

---
+++ flowerList.py
@@ -0,0 +1,16 @@
+flowerNames = ["gumamela", "lily", "sunflower", "daisy", "tulip", "orchid", "carnation", "iris", "peony", "jasmine"]
+
+print()
+print(flowerNames)
+print()
+print("The 5th index is: ", flowerNames[5])
+print()
+flowerNames[8] = "rose"
+flowerNames.remove(flowerNames[2])
+print()
+for x in flowerNames:
+    print(x)
+
+print()
+print("The last index is: ", flowerNames[-1])
+print()
