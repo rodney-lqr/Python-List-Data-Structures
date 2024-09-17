146dc050066f68fef659a657b6f843deda1171f1
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 23:49:35 +0800

    adding lists of social medias

---
+++ socialMediaList.py
@@ -0,0 +1,26 @@
+socialMedias = [
+    "Facebook",
+    "Threads",
+    "Twitter",
+    "TikTok",
+    "YouTube",
+    "Snapchat",
+    "LinkedIn",
+    "Reddit",
+    "Pinterest",
+    "WeChat"
+]
+
+print()
+print(socialMedias)
+print()
+print("The 5th index is: ", socialMedias[4])
+print()
+socialMedias[2] = "Instagram"
+del socialMedias[7]
+for x in socialMedias:
+    print(x)
+
+print()
+print("The last index is: ", socialMedias[-1])
+print()
