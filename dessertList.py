8913050e163ff1b15ad40c257203f421d78b3839
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 23:32:18 +0800

    adds the list of desserts

---
+++ dessertList.py
@@ -0,0 +1,26 @@
+desserts = [
+    "Ice cream",
+    "Cake",
+    "Chocolate chip cookies",
+    "Apple pie",
+    "Cheesecake",
+    "Tiramisu",
+    "Crème brûlée",
+    "Brownies",
+    "Pancakes",
+    "Donuts"
+]
+print()
+print(desserts)
+print()
+print("The 4th index is: ", desserts[3])
+print()
+desserts[5] = "Chocolate Cake"
+print()
+del desserts[2]
+for x in desserts:
+    print(x)
+
+print()
+print("The last index is: ", desserts[-1])
+print()
