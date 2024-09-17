fb9f1772a843b13617b323c8c1dd9e6ba7113a30
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-18 00:00:10 +0800

    adding a music genre list

---
+++ musicGenreList.py
@@ -0,0 +1,31 @@
+musicGenres = [
+    "Pop",
+    "Rock",
+    "Hip hop",
+    "Electronic",
+    "Country",
+    "Classical",
+    "R&B",
+    "Blues",
+    "Reggae",
+    "Metal",
+    "Punk",
+    "Soul",
+    "Gospel",
+    "Folk",
+    "Indie"
+]
+
+print()
+print(musicGenres)
+print()
+print(musicGenres[9])
+musicGenres[4] = "Jazz"
+del musicGenres[6]
+for x in musicGenres:
+    print(x)
+print()
+print(musicGenres[2:7])
+print()
+print(musicGenres[-1])
+print()
