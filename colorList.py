1436765f2e27da375a108d4a8f74baab8126a32b
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 19:51:36 +0800

    add a color list

---
+++ colorList.py
@@ -0,0 +1,30 @@
+colorList = [
+    'Red',
+    'Blue',
+    'Green',
+    'Yellow',
+    'Skyblue',
+    'Orange',
+    'Pink',
+    'Brown',
+    'Cyan',
+    'Magenta',
+    'Turquoise',
+    'Lavender',
+    'Olive',
+    'Maroon',
+    'Teal'
+]
+
+print()
+print(colorList)
+print()
+print("The 10th index is: ", colorList[10])
+print()
+colorList[4] = "Purple"
+colorList.remove(colorList[5])
+print()
+print(colorList[2:8])
+print()
+print(colorList)
+print()
