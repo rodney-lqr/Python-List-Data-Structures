6ac59d831c31f81285674becf090188087e64be9
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 19:45:15 +0800

    Added an animal list and modify some of the items

---
+++ animalList.py
@@ -0,0 +1,25 @@
+animalList = [
+    'Koala',
+    'Elephant',
+    'Crocodile',
+    'Wolf',
+    'Lion',
+    'Fox',
+    'Tiger',
+    'Panda',
+    'Kangaroo',
+    'Dolphin'
+]
+
+print()
+print(animalList)
+print()
+print("The third index is: ", animalList[3])
+print()
+animalList[6] = "Elephant"
+animalList.remove(animalList[8])
+print()
+print(animalList)
+print()
+print(animalList[-1])
+print()
