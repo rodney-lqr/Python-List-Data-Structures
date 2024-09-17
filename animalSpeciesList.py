a862f5365160ea6e175ab9d2b23b645e329ae98c
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 22:59:15 +0800

    adds the list of animal species

---
+++ animalSpeciesList.py
@@ -0,0 +1,24 @@
+animalSpecies = [
+    "Lion",
+    "Tiger",
+    "Elephant",
+    "Giraffe",
+    "Zebra",
+    "Panda",
+    "Dolphin"
+]
+
+print()
+print(animalSpecies)
+print()
+print("The second index is: ", animalSpecies[1])
+print()
+animalSpecies[4] = "Gorilla"
+print()
+del animalSpecies[5]
+for x in animalSpecies:
+    print(x)
+
+print()
+print("The last index is: ", animalSpecies[-1])
+print()
