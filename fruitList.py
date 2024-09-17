512acf62197d7971ce23432640026514931b11fe
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 19:58:10 +0800

    modified the colorList and added a fruitlist file

---
+++ fruitList.py
@@ -0,0 +1,28 @@
+fruitList = [
+    'Apple',
+    'Banana',
+    'Orange',
+    'Guava',
+    'Pineapple',
+    'Strawberry',
+    'Grapes',
+    'Watermelon',
+    'Peach',
+    'Cherry',
+    'Blueberry',
+    'Kiwi'
+]
+
+print()
+print(fruitList)
+print()
+print(fruitList[9])
+print()
+fruitList[2] = "Mango"
+print()  
+fruitList.remove(fruitList[10])
+print()
+print(fruitList[4:7])
+print()
+print(fruitList[-1])
+print()
