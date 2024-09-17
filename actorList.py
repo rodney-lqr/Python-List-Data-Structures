b152d8590ab19f74bbcc1ddba1aa28334e25ce7b
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-18 00:13:00 +0800

    adds a lists of famous actors and actresses in the world

---
+++ actorList.py
@@ -0,0 +1,27 @@
+famousActors = [
+    "Tom Hanks",
+    "Meryl Streep",
+    "Brad Pitt",
+    "Scarlett Johansson",
+    "Robert Downey Jr.",
+    "Nicole Kidman",
+    "Denzel Washington",
+    "Natalie Portman",
+    "Will Smith",
+    "Julia Roberts"
+]
+
+print()
+print(famousActors)
+print()
+print(famousActors[6])
+print()
+famousActors[2] = "Leonardo Di Caprio"
+del famousActors[7]
+for x in famousActors:
+    print(x)
+
+print()
+print(famousActors[-1])
+print()
+ 
