48bcef0e8238a120212a39325866ad0f66cb3329
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 22:38:28 +0800

    Added the lists of mountain names

---
+++ mountainList.py
@@ -0,0 +1,15 @@
+mountainNames = ["Mount Apo", "Mount Pulag", "Mount Kitanglad", "Mount Makiling", "Mount Banahaw", "Mount Mayon", "Mount Kanlaon", "Mount Isarog"]
+print()
+print(mountainNames)
+print()
+print("This is the 5th index ", mountainNames[4])
+print()
+mountainNames[2] = "Everest"
+print()
+del mountainNames[5]
+for x in mountainNames:
+    print(x)
+
+print()
+print("The last index is: ", mountainNames[-1])
+print()
