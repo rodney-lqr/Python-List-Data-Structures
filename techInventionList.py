12f24cc2ae5d054e808805e7f15c2348ee454b59
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-18 09:22:14 +0800

    Adds the lists of technology inventions through the years

---
+++ techInventionList.py
@@ -0,0 +1,25 @@
+modernInventions = [
+    "Electric Vehicle (EV)",
+    "Drone", "Virtual Reality (VR)",
+    "Augmented Reality (AR)",
+    "Artificial Intelligence (AI)", "3D Printer",
+    "Self-Driving Car",
+    "Renewable Energy Sources", "Blockchain",
+    "Wearable Technology",
+    "Quantum Computing", "Gene Editing",
+    "Space Exploration",
+    "Biotechnology", "Internet of Things (IoT)"
+]
+
+print()
+print(modernInventions)
+print()
+print("The 11th index is: ", modernInventions[10])
+print()
+modernInventions[7] = "Smartphone"
+del modernInventions[13]
+print()
+print(modernInventions[4:9])
+print()
+print(modernInventions[-1])
+print()
