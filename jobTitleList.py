b05dec62a6bc52ce41dd9b0dd6937d437369b1fa
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-18 09:30:02 +0800

    adding a job title list

---
+++ jobTitleList.py
@@ -0,0 +1,26 @@
+jobTitles = [
+    "Full Stack Developer",
+    "Data Scientist",
+    "Project Manager",
+    "Marketing Specialist",
+    "Graphic Designer",
+    "Accountant",
+    "Teacher",
+    "Nurse",
+    "Doctor",
+    "Lawyer"
+]
+
+print()
+print(jobTitles)
+print()
+print("The 6th index is: ", jobTitles[5])
+jobTitles[4] = "Software Engineer"
+print()
+del jobTitles[8]
+for x in jobTitles:
+    print(x)
+
+print()
+print("The last index is: ", jobTitles[-1])
+print()
