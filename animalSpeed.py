3ab8b052f094bd36a28d47ffc544437d8e3a7240
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-18 10:46:27 +0800

    adding a lists of animal speeds

---
+++ animalSpeed.py
@@ -0,0 +1,27 @@
+landAnimalsSpeed = [
+    "Gorilla 25 km/h",
+    "Horse 70 km/h",
+    "Elephant 40 km/h",
+    "Kangaroo 70 km/h",
+    "Gazelle 100 km/h",
+    "Lion 80 km/h",
+    "Giraffe 60 km/h",
+    "Bear 40 km/h",
+    "Rabbit 55 km/h",
+    "Squirrel 20 km/",
+    "Deer 80 km/",
+    "Snake 5 km/"
+]
+
+print()
+print(landAnimalsSpeed)
+print()
+print("The 7th index is: ", landAnimalsSpeed[6])
+print()
+landAnimalsSpeed[8] = "Cheetah - 120 km/h"
+del landAnimalsSpeed[9]
+print()
+print(landAnimalsSpeed[2:6])
+print()
+print(landAnimalsSpeed[-1])
+print()
